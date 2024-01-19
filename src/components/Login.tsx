07722b667cb660fc78391f76482f4902ccd7eac6
Nurul Azizah <idlionora@gmail.com>
2024-01-19 21:06:29 +0700

    feat(signup): add form in signup component, move auth titles to astro file

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,39 +3,41 @@ import { useState } from 'react';
 
 const Login = () => {
 	const [email, setEmail] = useState('');
-	const [password, setpassword] = useState('');
-	const [submittedForm, setSubmittedForm] = useState({email: '', password: ''})
+	const [password, setPassword] = useState('');
+	const [submittedForm, setSubmittedForm] = useState({ email: '', password: '' });
+	const [isLoading, setIsLoading] = useState(false);
 	const [loginStatus, setLoginStatus] = useState('');
 
 	async function submitForm(event: React.FormEvent) {
 		event.preventDefault();
-		setSubmittedForm({email, password})
+		setSubmittedForm({ email, password });
 		try {
-			setLoginStatus(`${loginStatus} loading`);
+			setIsLoading(true);
 			let res = await axios.post(`${import.meta.env.PUBLIC_APISITE}/api/v1/users/login`, {
 				email,
 				password,
 			});
 			res = res.data;
 			console.log(res.status);
+			setIsLoading(false);
 			setLoginStatus('success');
 		} catch (error) {
 			const err = error as AxiosError;
+			setIsLoading(false);
 			if (err.response?.status === 401) {
 				setLoginStatus('incorrectFill');
 			}
 		}
 	}
 
-	if (loginStatus === "incorrectFill" && email !== submittedForm.email) setLoginStatus('isCorrecting');
-	if (loginStatus === 'incorrectFill' && password !== submittedForm.password) setLoginStatus('isCorrecting');
+	if (loginStatus === 'incorrectFill' && email !== submittedForm.email)
+		setLoginStatus('isCorrecting');
+	if (loginStatus === 'incorrectFill' && password !== submittedForm.password)
+		setLoginStatus('isCorrecting');
 
 	return (
-		<section className="auth-container" aria-label="Form to log in">
-			<h2 className="text-white font-heading font-bold leading-8 text-[26px] mb-4">
-				Log in to myMiniBlog
-			</h2>
-			{loginStatus.includes('incorrectFill') || loginStatus.includes("isCorrecting") ? (
+		<>
+			{loginStatus === 'incorrectFill' || loginStatus === 'isCorrecting' ? (
 				<div className="statusmsg fail">Email or password is invalid!</div>
 			) : (
 				''
@@ -63,18 +65,18 @@ const Login = () => {
 					className={`auth-input ${loginStatus === 'incorrectFill' ? 'border-2 border-rose-400 focus:outline-rose-500' : 'focus:outline-blue-800'}`}
 					placeholder="********"
 					required
-					onChange={(e) => setpassword(e.target.value)}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<div className="flex justify-center mt-3.5 relative">
 					<button
 						type="submit"
-						className={`auth-button ${loginStatus === 'loading' ? 'auth-loading' : ''}`}
+						className={`auth-button ${isLoading ? 'auth-loading' : ''}`}
 					>
-						{loginStatus === 'loading' ? 'Please wait...' : 'Log in'}
+						{isLoading ? 'Please wait...' : 'Log in'}
 					</button>
 				</div>
 			</form>
-		</section>
+		</>
 	);
 };
 
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,8 +1,80 @@
+import { useState } from 'react';
+
 const Signup = () => {
+	const [formInput, setFormInput] = useState({
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirm: '',
+	});
+	const [isLoading, setIsLoading] = useState(false);
+
+	function submitForm(event: React.FormEvent) {
+		event.preventDefault();
+	}
 	return (
-		<main>
-			<section>Create an account</section>
-		</main>
+		<>
+			<form className="w-full" onSubmit={(e) => submitForm(e)}>
+				<label htmlFor="name" className="auth-label">
+					Username
+				</label>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					className={`auth-input`}
+					placeholder="John Doe"
+					required
+					onChange={(e) => setFormInput({ ...formInput, name: e.target.value })}
+				/>
+				<label htmlFor="email" className="auth-label">
+					Email
+				</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					className={`auth-input`}
+					placeholder="johndoe@email.com"
+					required
+					onChange={(e) => setFormInput({ ...formInput, email: e.target.value })}
+				/>
+				<label htmlFor="password" className="auth-label">
+					Password
+				</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					className={`auth-input`}
+					placeholder="********"
+					required
+					onChange={(e) => setFormInput({ ...formInput, password: e.target.value })}
+				/>
+				<label htmlFor="passwordConfirm" className="auth-label">
+					Confirm Password
+				</label>
+				<input
+					type="password"
+					name="passwordConfirm"
+					id="passwordConfirm"
+					className={`auth-input`}
+					placeholder="********"
+					required
+					onChange={(e) =>
+						setFormInput({ ...formInput, passwordConfirm: e.target.value })
+					}
+				/>
+				<div className="flex justify-start mt-3.5 relative">
+					<button
+						type="submit"
+						className={`auth-button ${isLoading ? 'auth-loading' : ''}`}
+					>
+						{isLoading ? 'Please wait...' : 'Sign up'}
+					</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
