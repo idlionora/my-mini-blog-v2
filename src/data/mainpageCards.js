b63d6d8e05e008f013d0c55db4e0b08cf0904d7d
Nurul Azizah <idlionora@gmail.com>
2024-02-18 20:07:06 +0700

    feat(index): add BlogCard and cardData into index.astro

---
+++ src/data/mainpageCards.js
@@ -0,0 +1,28 @@
+const cardData = [
+	{
+		_id: '65d1f701cdf5b817ea64f3bb',
+		title: 'Nonreturnable Fees and Grey Area of Spending',
+		summary:
+			'A moment of confusion when it comes to reporting the election committee’s operational budget.',
+		slug: 'nonreturnable-fees-and-grey-area-of-spending',
+		blogpostImg:
+			'/v1708259080/my-mini-blog/blogpost_img/blogpost-65d1f701cdf5b817ea64f3bb_2024-02-18-228.jpg',
+		blogthumbImg:
+			'/v1708259082/my-mini-blog/blogthumb_img/blogthumb-65d1f701cdf5b817ea64f3bb_2024-02-18-926.jpg',
+		bannerImg:
+			'/v1708259078/my-mini-blog/banner_img/banner-65d1f701cdf5b817ea64f3bb_2024-02-18-722.jpg',
+		content:
+			'It has been two days since the grand election day of Indonesia. At the time, I was given the opportunity to be part of the election committee in my neighborhood. The event itself was tiring but fun! I got to wrestle with at least 70 sheets of A1 papers and input its data into this app with who knows how many committees out there from Sabang to Marauke (There were 820.161 polling stations if google were accurate). But hey! Chaotic queue aside, I wanna talk about something else that had been bothering me on the weekend before election day!',
+		tags: ['election', 'teamwork', 'real life experience', 'managing money'],
+		createdAt: '2024-02-18T12:08:46.028Z',
+		updatedAt: '2024-02-18T12:08:46.028Z',
+		user: {
+			_id: '655c7a2bd2ffdd11ea492d1c',
+			name: 'Barry T. Quokka',
+		},
+		commentCount: 0,
+		id: '65d1f701cdf5b817ea64f3bb',
+	},
+];
+
+export default cardData;
