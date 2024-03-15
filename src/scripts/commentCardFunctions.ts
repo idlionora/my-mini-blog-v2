be98089e68335a1bc8ef1135aece3172829f7d88
Nurul Azizah <idlionora@gmail.com>
2024-03-16 00:41:11 +0700

    feat([savedSlug]): add assignDisabledButtons, build pagination, and loadTabNum for CommentSection class, add switchToTabNum and paginationInit functions

--- src/scripts/commentCardFunctions.ts
+++ src/scripts/commentCardFunctions.ts
@@ -15,65 +15,102 @@ export interface CommentData {
     user: {_id: string, name: string, photo: string}
 }
 
+export async function switchToTabNum(commentClass:CommentSection, newTabNum:number) {
+    commentClass.loadTabNum(newTabNum);
+    document.getElementById('pagination-first')?.addEventListener('click', () => switchToTabNum(commentClass, 1));
+    document
+		.getElementById('pagination-last')
+		?.addEventListener('click', () => switchToTabNum(commentClass, commentClass.commentTabNum));
+    //handle addEventListener for edit and delete button    
+}
+
 class CommentSection {
-    protected blogId: string;
-    protected commentContainer: HTMLElement;
-    protected paginationContainer: HTMLElement;
+	protected blogId: string;
+	paginationContainer: HTMLElement;
+	paginationNumContainer: HTMLElement;
+	commentContainer: HTMLElement;
 
-    fetchedComments: CommentData[] = [];
-    commentNumPerTab: number = 5
-    commentTabNum: number = 1;
-    currentTab: number = 1;
+	protected fetchedComments: CommentData[] = [];
+	commentNumPerTab: number = 5;
+	commentTabNum: number = 1;
+	currentTab: number = 1;
 
-    constructor(blogId: string, commentContainer: HTMLElement, paginationContainer: HTMLElement) {
-        this.blogId = blogId;
-        this.commentContainer = commentContainer;
-        this.paginationContainer = paginationContainer;
-    }
+	constructor(blogId: string, paginationContainer: HTMLElement, commentContainer: HTMLElement) {
+		this.blogId = blogId;
+		this.paginationContainer = paginationContainer;
+		this.paginationNumContainer = paginationContainer.children[1] as HTMLElement;
+		this.commentContainer = commentContainer;
+	}
 
-    displayPageNums () {
-        console.log(this.commentTabNum);
-        console.log(this.commentNumPerTab);
-        console.log(this.currentTab)
-        const pagesArr:string[] = []
-        const firstTab = '<button id="pagination-first" class="button-pagination">1</button>'
-        const currentTab = `<button class="button-pagination-selected">${this.currentTab}</button>`
-        const lastTab = `<button id="pagination-last" class="button-pagination">${this.commentTabNum}</button>`;
-        const dotsTab = '<div class="button-pagination-dots">...</div>'
+	async fetchComments() {
+		try {
+			const res = await axios.get(
+				`${import.meta.env.PUBLIC_APISITE}/blogposts/${this.blogId}/comments`
+			);
+			const fetchedComments = res.data.data;
+			this.fetchedComments = fetchedComments;
+			this.commentTabNum = Math.ceil(fetchedComments.length / this.commentNumPerTab);
+		} catch (error) {
+			const err = error as AxiosError;
+			console.log(err.message);
+		}
+	}
 
-        if (this.currentTab !== 1) pagesArr.push(firstTab);
-        if (this.currentTab > 2) pagesArr.push(dotsTab);
-        pagesArr.push(currentTab);
-        if (this.commentTabNum - 1 > this.currentTab) pagesArr.push(dotsTab);
-        if (this.commentTabNum > this.currentTab) pagesArr.push(lastTab);
-        console.log(this.pagesArr)
+	displayPageNums() {
+		const pagesArr: string[] = [];
+		const firstTab = '<button id="pagination-first" class="button-pagination">1</button>';
+		const currentTab = `<button id="pagination-selected" class="button-pagination-selected">${this.currentTab}</button>`;
+		const lastTab = `<button id="pagination-last" class="button-pagination">${this.commentTabNum}</button>`;
+		const dotsTab = '<div class="button-pagination-dots">...</div>';
 
-        const parsedTabs = new DOMParser().parseFromString(pagesArr.join(''), 'text/html').body;
-        this.paginationContainer.innerHTML = parsedTabs.innerHTML; 
-        console.log(this.paginationContainer.innerHTML)        
-    }
+		if (this.currentTab !== 1) pagesArr.push(firstTab);
+		if (this.currentTab > 2) pagesArr.push(dotsTab);
+		pagesArr.push(currentTab);
+		if (this.commentTabNum - 1 > this.currentTab) pagesArr.push(dotsTab);
+		if (this.commentTabNum > this.currentTab) pagesArr.push(lastTab);
 
-    async fetchComments() {
-        try {
-            const res = await axios.get(`${import.meta.env.PUBLIC_APISITE}/blogposts/${this.blogId}/comments`);
-            const fetchedComments = res.data.data;
-            this.fetchedComments = fetchedComments
-            this.currentTab = 3;
-            this.commentTabNum = Math.ceil(fetchedComments.length/ this.commentNumPerTab);
+		const parsedTabs = new DOMParser().parseFromString(pagesArr.join(''), 'text/html').body;
+		this.paginationNumContainer.innerHTML = parsedTabs.innerHTML;
+	}
 
-            this.displayPageNums()
-        } catch(error) {
-            const err = error as AxiosError;
-            console.log(err.message);
+	assignDisabledButtons() {
+		const numContainerLength = this.paginationNumContainer.children.length;
+		if (this.currentTab === 1) {
+			this.paginationContainer.children[0].setAttribute('disabled', '');
+			this.paginationNumContainer.children[0].setAttribute('disabled', '');
+		} else {
+			this.paginationContainer.children[0].removeAttribute('disabled');
+		}
+
+		if (this.currentTab === this.commentTabNum) {
+			this.paginationContainer.children[2].setAttribute('disabled', '');
+			this.paginationNumContainer.children[numContainerLength - 1].setAttribute(
+				'disabled',
+				''
+			);
+		} else {
+			this.paginationContainer.children[2].removeAttribute('disabled');
+		}
+	}
+
+	async buildPagination(tabJump: "first" | "last" = "first") {
+		await this.fetchComments();
+        if (tabJump === "first") {
+            this.currentTab = 1;
+        } else if (tabJump === "last") {
+            this.currentTab = this.commentTabNum;
         }
-    }
+		this.displayPageNums();
+		this.assignDisabledButtons();
+        //displayComments()
+	}
 
-
-    buildPagination () {
-        
-    }
-
-    switchTabNum() {}
+    loadTabNum(newTabNum: number) {
+		this.currentTab = newTabNum;
+		this.displayPageNums();
+		this.assignDisabledButtons();
+		//displayComments()
+	}
 
 }
 
